e46bd3a6818445bc86b66be891025f91750befa5
Emilie <EmilieMontpre@outlook.com>
2023-04-21 12:59:10 +0200

    all route and new page (ClubsPage)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
-import Ligues from '../views/Ligues.vue';
+import LiguesPage from '../views/LiguesPage.vue';
+import ClubsPage from '../views/ClubsPage.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,28 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+
+
+  {
+    path: '/ligues/:id',
+    redirect: '/ligues/:id'
+  },
+  {
+    path: '/ligues/:id',
+    name: 'Ligues',
+    component: LiguesPage
+  },
+
+
+  {
+    path: '/clubs/:id',
+    redirect: '/clubs/:id'
+  },
+  {
+    path: '/clubs/:id',
+    name: 'Clubs',
+    component: ClubsPage
   }
 ]
 
