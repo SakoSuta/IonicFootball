44d713eec6610965a001a3a3c4374eb00262745d
Emilie <EmilieMontpre@outlook.com>
2023-04-21 12:27:07 +0200

    add : Ion-Card

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,52 @@
 <template>
-  
+  <div class="AllCard">
+      <ion-card button routerLink="/chemin-de-la-page">
+        <ion-card-header>
+            <ion-card-title>French Ligue 1</ion-card-title>
+            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+            <!-- {{ content.substring(0, 50) }}{{ content.length > 50 ? '...' : '' }} -->
+            ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        </ion-card-content>
+      </ion-card>
+      <ion-card button routerLink="/chemin-de-la-page">
+        <ion-card-header>
+            <ion-card-title>French Ligue 1</ion-card-title>
+            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+            <!-- {{ content.substring(0, 50) }}{{ content.length > 50 ? '...' : '' }} -->
+            ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        </ion-card-content>
+      </ion-card>
+      <ion-card button routerLink="/chemin-de-la-page">
+        <ion-card-header>
+            <ion-card-title>French Ligue 1</ion-card-title>
+            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+            <!-- {{ content.substring(0, 50) }}{{ content.length > 50 ? '...' : '' }} -->
+            ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        </ion-card-content>
+      </ion-card>
+  </div>
 </template>
 
 <script setup lang="ts">
-import {  } from '@ionic/vue';
+import { IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent } from '@ionic/vue';
+
 </script>
 
 <style scoped>
-
+.AllCard{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+ion-card{
+    width: 70%;
+}
 </style>
